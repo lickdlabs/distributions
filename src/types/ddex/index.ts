62b8eb2cc37e9af35ff38505e3fc31184b20f989
Gary Rutland <garyrutland@lickd.co>
2024-03-01 09:48:05 +0000

    Exported version types

--- src/types/ddex/index.ts
+++ src/types/ddex/index.ts
@@ -1,8 +1,14 @@
 import * as v3 from "./3";
 import * as v4 from "./4";
 
-export * as v3 from "./3";
-export * as v4 from "./4";
+export * as DdexV3 from "./3";
+export * as DdexV38 from "./3/8";
+export * as DdexV381 from "./3/8/1";
+export * as DdexV382 from "./3/8/2";
+export * as DdexV383 from "./3/8/3";
+
+export * as DdexV4 from "./4";
+export * as DdexV411 from "./4/1/1";
 
 export type TDistroDdexVersion = v3.TDistroDdexVersion | v4.TDistroDdexVersion;
 
