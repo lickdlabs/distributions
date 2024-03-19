6dfb015647c5994e0d86c1c886b78cce50e3f214
Gary Rutland <garyrutland@lickd.co>
2024-03-19 21:45:52 +0000

    Added top level element

--- src/types/ddex/ern/index.ts
+++ src/types/ddex/ern/index.ts
@@ -1,3 +1,9 @@
+import * as Ern382 from "./382";
+import * as Ern383 from "./383";
+import * as Ern411 from "./411";
+
 export * as Ern382 from "./382";
 export * as Ern383 from "./383";
 export * as Ern411 from "./411";
+
+export type Element = Ern382.Element | Ern383.Element | Ern411.Element;
