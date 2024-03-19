8371cd27cdee3b715a4526fb2db20f76bd66b222
Gary Rutland <garyrutland@lickd.co>
2024-03-19 21:43:04 +0000

    Added element type

--- src/types/ddex/ern/383/elements/index.ts
+++ src/types/ddex/ern/383/elements/index.ts
@@ -1,3 +1,12 @@
+import { CatalogListMessage } from "./catalogListMessage";
+import { NewReleaseMessage } from "./newReleaseMessage";
+import { PurgeReleaseMessage } from "./purgeReleaseMessage";
+
 export * from "./catalogListMessage";
 export * from "./newReleaseMessage";
 export * from "./purgeReleaseMessage";
+
+export type Element =
+  | CatalogListMessage
+  | NewReleaseMessage
+  | PurgeReleaseMessage;
--- src/types/ddex/ern/411/elements/index.ts
+++ src/types/ddex/ern/411/elements/index.ts
@@ -1,2 +1,7 @@
+import { NewReleaseMessage } from "./newReleaseMessage";
+import { PurgeReleaseMessage } from "./purgeReleaseMessage";
+
 export * from "./newReleaseMessage";
 export * from "./purgeReleaseMessage";
+
+export type Element = NewReleaseMessage | PurgeReleaseMessage;
