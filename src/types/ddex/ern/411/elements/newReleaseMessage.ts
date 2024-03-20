c363e56712e833fce4314b9d8c60aa4a6de935f4
Gary Rutland <garyrutland@lickd.co>
2024-03-20 09:16:46 +0000

    Added 411 comments

--- src/types/ddex/ern/411/elements/newReleaseMessage.ts
+++ src/types/ddex/ern/411/elements/newReleaseMessage.ts
@@ -1,6 +1,23 @@
 import { Avs411 } from "../../../avs";
 import { MessageHeader } from "../complexTypes";
 
+// <xs:element name="NewReleaseMessage">
+//   <xs:complexType>
+//     <xs:sequence>
+//       <xs:element name="MessageHeader" type="ern:MessageHeader" />
+//       <xs:element name="PartyList" type="ern:PartyList" />
+//       <xs:element name="CueSheetList" minOccurs="0" type="ern:DetailedCueSheetList" />
+//       <xs:element name="ResourceList" type="ern:ResourceList" />
+//       <xs:element name="ChapterList" minOccurs="0" type="ern:ChapterList" />
+//       <xs:element name="ReleaseList" type="ern:ReleaseList" />
+//       <xs:element name="DealList" minOccurs="0" type="ern:DealList" />
+//       <xs:element name="SupplementalDocumentList" minOccurs="0" type="ern:SupplementalDocumentList" />
+//     </xs:sequence>
+//     <xs:attribute name="ReleaseProfileVersionId" type="avs:ReleaseProfileVersionId" />
+//     <xs:attribute name="ReleaseProfileVariantVersionId" type="avs:ReleaseProfileVariantVersionId" />
+//     <xs:attribute name="LanguageAndScriptCode" type="xs:string" use="required" />
+//   </xs:complexType>
+// </xs:element>
 export type NewReleaseMessage = {
   _attributes: {
     releaseProfileVersionId?: Avs411.ReleaseProfileVersionId;
@@ -9,11 +26,11 @@ export type NewReleaseMessage = {
   };
 
   messageHeader: MessageHeader;
-  // <xs:element name="PartyList" type="ern:PartyList" />
-  // <xs:element name="CueSheetList" minOccurs="0" type="ern:DetailedCueSheetList" />
-  // <xs:element name="ResourceList" type="ern:ResourceList" />
-  // <xs:element name="ChapterList" minOccurs="0" type="ern:ChapterList" />
-  // <xs:element name="ReleaseList" type="ern:ReleaseList" />
-  // <xs:element name="DealList" minOccurs="0" type="ern:DealList" />
-  // <xs:element name="SupplementalDocumentList" minOccurs="0" type="ern:SupplementalDocumentList" />
+  // @todo <xs:element name="PartyList" type="ern:PartyList" />
+  // @todo <xs:element name="CueSheetList" minOccurs="0" type="ern:DetailedCueSheetList" />
+  // @todo <xs:element name="ResourceList" type="ern:ResourceList" />
+  // @todo <xs:element name="ChapterList" minOccurs="0" type="ern:ChapterList" />
+  // @todo <xs:element name="ReleaseList" type="ern:ReleaseList" />
+  // @todo <xs:element name="DealList" minOccurs="0" type="ern:DealList" />
+  // @todo <xs:element name="SupplementalDocumentList" minOccurs="0" type="ern:SupplementalDocumentList" />
 };
