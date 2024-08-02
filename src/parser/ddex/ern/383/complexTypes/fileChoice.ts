0e68a33b967ad3eb73466b2ddfbf3b46614ebecf
Gary Rutland <garyrutland@lickd.co>
2024-08-02 10:11:41 +0100

    Corrected file choice

--- src/parser/ddex/ern/383/complexTypes/fileChoice.ts
+++ src/parser/ddex/ern/383/complexTypes/fileChoice.ts
@@ -4,27 +4,35 @@ import { parseFile } from "./file";
 
 export const parseFileChoice = (object: any): Ern383.FileChoice => {
   if (object.FileAvailabilityDescription) {
-    return object.FileAvailabilityDescription.map(
-      (fileAvailabilityDescription: any) =>
-        parseDescription(fileAvailabilityDescription),
-    );
+    return {
+      fileAvailabilityDescription: object.FileAvailabilityDescription.map(
+        (fileAvailabilityDescription: any) =>
+          parseDescription(fileAvailabilityDescription),
+      ),
+    };
   }
 
-  return object.File.map((file: any) => parseFile(file));
+  return {
+    file: object.File.map((file: any) => parseFile(file)),
+  };
 };
 
 export const parsePartialFileChoice = (
   object: any,
 ): Partial<Ern383.FileChoice> => {
   if (object.FileAvailabilityDescription) {
-    return object.FileAvailabilityDescription.map(
-      (fileAvailabilityDescription: any) =>
-        parseDescription(fileAvailabilityDescription),
-    );
+    return {
+      fileAvailabilityDescription: object.FileAvailabilityDescription.map(
+        (fileAvailabilityDescription: any) =>
+          parseDescription(fileAvailabilityDescription),
+      ),
+    };
   }
 
   if (object.File) {
-    return object.File.map((file: any) => parseFile(file));
+    return {
+      file: object.File.map((file: any) => parseFile(file)),
+    };
   }
 
   return {};
