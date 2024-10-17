e23532690ca59f6213df6f4185e50287b94628d1
Gary Rutland <garyrutland@lickd.co>
2024-10-17 13:49:26 +0100

    Update image.ts

--- src/normaliser/ddex/ern/411/complexTypes/image.ts
+++ src/normaliser/ddex/ern/411/complexTypes/image.ts
@@ -4,8 +4,16 @@ export const normaliseImage = (
   image: Ern411.Image,
   release: Ern411.Release,
 ): void => {
-  image.displayTitleText = release.displayTitleText;
-  image.displayTitle = release.displayTitle;
-  image.displayArtistName = release.displayArtistName;
-  image.displayArtist = release.displayArtist;
+  if (!image.displayTitleText) {
+    image.displayTitleText = release.displayTitleText;
+  }
+  if (!image.displayTitle) {
+    image.displayTitle = release.displayTitle;
+  }
+  if (!image.displayArtistName) {
+    image.displayArtistName = release.displayArtistName;
+  }
+  if (!image.displayArtist) {
+    image.displayArtist = release.displayArtist;
+  }
 };
